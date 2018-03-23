b0c96e8db35e4e84e92264cea7953ff546ad404e
z2x <zhufind@yeah.net>
2018-03-23 21:20:38 +0800

    delete webpack demo

--- web/webpack-demo/webpack.dev.js
+++
@@ -1,9 +0,0 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
-
-module.exports = merge(common, {
-  devtool: 'inline-sourse-map',
-  devServer: {
-    contentBase: './dist'
-  }
-});
